1d96c94b8989248e680041101658cea42e5a25c3
Rosine476 <109015499+Rosine476@users.noreply.github.com>
2022-07-20 16:59:59 +0800

    Add files via upload

--- chang.c
+++ chang.c
@@ -5,14 +5,13 @@ int main()
 {
 	int price=0;
 	
-	printf("10 :");
-	
+	printf("请输入金额（元）:");
 	
 	scanf("%d",&price );
 	
 	int change=100-price;
 	
-	printf("90.\n",change);
+	printf("找您%d元.\n",change);
 	
 	return 0;
 }
